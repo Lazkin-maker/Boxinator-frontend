0d27d618bf69660145a8ac66a2c3518f7d5ec545
Rubin Barclay <rubin.barclay@se.experis.com>
2023-03-07 10:50:52 +0100

    feat: routing with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+import Account from "./pages/Account/Account";
+import Admin from "./pages/Admin/Admin";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Shipping from "./pages/Shipping/Shipping";
 
 function App() {
   return (
-    <div className="App text-red-500">
-      <p>hello world</p>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Shipping />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Shipping/Shipping.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Shipping({}: Props) {
+  return (
+    <div>Shipping</div>
+  )
+}
+
+export default Shipping
