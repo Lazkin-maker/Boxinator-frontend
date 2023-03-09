138f11123f4ff316236ef95195d63fb711860643
Jakob-mbi <jakob_mbi@msn.com>
2023-03-09 17:56:06 +0100

    feat: view ShipmentList in admin page

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import AdminMobilNavbar from '../../components/Admin/AdminNavbar/AdminMobilNavbar'
 import AdminSidebar from '../../components/Admin/AdminNavbar/AdminSidebar'
+import ShipmentList from '../../components/ShipmentList/ShipmentList';
+import data from "./dummy.json";
 
 
 
 
 
 const Admin = () => {
+  const shipments = data.shipments;
   const [menu, setMenu] = useState(false);
 
   const showMenu = () => {
@@ -16,8 +19,12 @@ const Admin = () => {
     <div className='relative min-h-screen bg-color-black text-white md:flex'>
       <AdminMobilNavbar showMenu={showMenu}/>
       <AdminSidebar menu={menu}/>
-      <div className='flex-1 p-10 text-2xl font-bold'>
-        <input className='shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-2/4 ' type="text" placeholder="Username"></input>
+      <div className='flex-1 p-10 text-2xl font-bold flex items-center justify-center'>
+        <div className='w-5/6 h-5/6'>
+          <input className='shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-2/4 ' type="text" placeholder="Shipment ID"></input>
+          <h2 className="text-2xl font-bold mt-5">Shipments</h2>
+          <ShipmentList shipments={shipments} />
+        </div>
       </div>
     </div>
   )
