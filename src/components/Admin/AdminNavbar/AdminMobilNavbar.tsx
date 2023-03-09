10bcc1dc2502b3b9d1fe479d19bdf7551f3fcebb
Jakob-mbi <jakob_mbi@msn.com>
2023-03-09 17:11:52 +0100

    feat: Responsiv AdminNavbar

---
+++ src/components/Admin/AdminNavbar/AdminMobilNavbar.tsx
@@ -0,0 +1,21 @@
+type PropsType = {
+    showMenu: () => void
+}
+
+const AdminMobilNavbar = ({showMenu}:PropsType) => {
+    
+    return (
+      <div className='flex justify-between md:hidden bg-gray-700'>
+            <a href='#' className='block p-4 font-extrabold text-2xl'>
+                Admin
+            </a>
+            <button className="mobile-menu-button p-4 focus:outline-none focus:bg-blue-900 rounded m-1" onClick={() => showMenu()}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </button>
+      </div>
+    )
+  }
+  
+  export default AdminMobilNavbar
--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import AdminNavbar from '../../components/Admin/AdminNavbar'
-import PackageList from '../../components/Packages/PackageList'
+import React, { useState } from 'react'
+import AdminMobilNavbar from '../../components/Admin/AdminNavbar/AdminMobilNavbar'
+import AdminSidebar from '../../components/Admin/AdminNavbar/AdminSidebar'
 
 
 
-function Admin() {
+
+
+const Admin = () => {
+  const [menu, setMenu] = useState(false);
+
+  const showMenu = () => {
+    menu ? setMenu(false) : setMenu(true);
+  }
   return (
-    <div className='grid grid-cols-3'>
-      <AdminNavbar />
-      <div className='flex justify-center mt-11'>
+    <div className='relative min-h-screen bg-color-black text-white md:flex'>
+      <AdminMobilNavbar showMenu={showMenu}/>
+      <AdminSidebar menu={menu}/>
+      <div className='flex-1 p-10 text-2xl font-bold'>
         <input className='shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-2/4 ' type="text" placeholder="Username"></input>
       </div>
-        <PackageList />
     </div>
   )
 }
