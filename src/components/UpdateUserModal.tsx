b75a395bf30a3437b3172f5b488dadf459ad6561
Lazkin Youssuf <lazkin.youssuf96@yahoo.com>
2023-03-11 21:07:12 +0100

    fix : fixed account page

---
+++ src/components/UpdateUserModal.tsx
@@ -0,0 +1,100 @@
+import { Dispatch, SetStateAction, useState } from "react"
+import user from '../user.json'
+
+
+
+
+type Props = {
+  closeModal: () => void,
+  setShowConfirmationModal: Dispatch<SetStateAction<boolean>>
+}
+
+function UpdateUserModal({ closeModal, setShowConfirmationModal }: Props) {
+  const [userData, setUserData] = useState(user);
+  const [dateOfBirth, setDateOfBirth] = useState(userData.dateOfBirth);
+  const [countryOfResidence, setCountryOfResidence] = useState(userData.countryOfResidence);
+  const [zipCode, setZipCode] = useState(userData.zipCode);
+  const [contactNumber, setContactNumber] = useState(userData.contactNumber);
+
+  return (
+    <>
+      <div className="mt-4">
+        <p className="font-medium mb-1">Date of Birth:</p>
+        <input
+          className="border border-gray-300 p-2 rounded-md w-full"
+          type="date"
+          value={dateOfBirth}
+          onChange={(e) => setDateOfBirth(e.target.value)}
+        />
+      </div>
+
+      <div className="mt-4">
+        <p className="font-medium mb-1">Country of Residence:</p>
+        <input
+          className="border border-gray-300 p-2 rounded-md w-full"
+          type="text"
+          value={countryOfResidence}
+          onChange={(e) => setCountryOfResidence(e.target.value)}
+        />
+      </div>
+
+      <div className="mt-4">
+        <p className="font-medium mb-1">Zip Code/Postal Code:</p>
+        <input
+          className="border border-gray-300 p-2 rounded-md w-full"
+          type="text"
+          value={zipCode}
+          onChange={(e) => setZipCode(e.target.value)}
+        />
+      </div>
+
+      <div className="mt-4">
+        <p className="font-medium mb-1">Contact Number:</p>
+        <input
+          className="border border-gray-300 p-2 rounded-md w-full"
+          type="text"
+          value={contactNumber}
+          onChange={(e) => setContactNumber(e.target.value)}
+        />
+      </div>
+
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => {
+          const updatedUserData = {
+            ...userData,
+            dateOfBirth: dateOfBirth,
+            countryOfResidence: countryOfResidence,
+            zipCode: zipCode,
+            contactNumber: contactNumber
+          };
+
+          fetch('/api/saveUser', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedUserData)
+          })
+            .then(response => {
+              if (!response.ok) {
+                throw new Error('Failed to save user information.');
+              }
+              console.log('User information saved.');
+            })
+            .catch(error => {
+              console.error(error);
+            });
+        }}
+      >
+        Save
+      </button>
+      <button onClick={closeModal} className="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm">
+        Cancel
+      </button>
+      
+    </>
+  )
+}
+
+export default UpdateUserModal
--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import keycloak from '../../keycloak'
 import { useEffect, useState } from "react";
 import user from '../../user.json'
+import Update from '../../components/UpdateUserModal'
+import UpdateUserModal from '../../components/UpdateUserModal';
+import ConfirmationModal from '../../components/UpdateConfirmation';
 // import { getUserData } from "your-api-utils";
 
 
@@ -13,6 +16,8 @@ function Account({ }: Props) {
   const [countryOfResidence, setCountryOfResidence] = useState(userData.countryOfResidence);
   const [zipCode, setZipCode] = useState(userData.zipCode);
   const [contactNumber, setContactNumber] = useState(userData.contactNumber);
+  const [showUpdateUser, setShowUpdateUser] = useState(false);
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
 
 
 
@@ -61,78 +66,21 @@ function Account({ }: Props) {
         </div>
       )}
 
-      <div className="mt-4">
-        <p className="font-medium mb-1">Date of Birth:</p>
-        <input
-          className="border border-gray-300 p-2 rounded-md w-full"
-          type="date"
-          value={dateOfBirth}
-          onChange={(e) => setDateOfBirth(e.target.value)}
-        />
-      </div>
-
-      <div className="mt-4">
-        <p className="font-medium mb-1">Country of Residence:</p>
-        <input
-          className="border border-gray-300 p-2 rounded-md w-full"
-          type="text"
-          value={countryOfResidence}
-          onChange={(e) => setCountryOfResidence(e.target.value)}
-        />
-      </div>
-
-      <div className="mt-4">
-        <p className="font-medium mb-1">Zip Code/Postal Code:</p>
-        <input
-          className="border border-gray-300 p-2 rounded-md w-full"
-          type="text"
-          value={zipCode}
-          onChange={(e) => setZipCode(e.target.value)}
-        />
-      </div>
-
-      <div className="mt-4">
-        <p className="font-medium mb-1">Contact Number:</p>
-        <input
-          className="border border-gray-300 p-2 rounded-md w-full"
-          type="text"
-          value={contactNumber}
-          onChange={(e) => setContactNumber(e.target.value)}
-        />
-      </div>
-
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => {
-          const updatedUserData = {
-            ...userData,
-            dateOfBirth: dateOfBirth,
-            countryOfResidence: countryOfResidence,
-            zipCode: zipCode,
-            contactNumber: contactNumber
-          };
-
-          fetch('/api/saveUser', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(updatedUserData)
-          })
-            .then(response => {
-              if (!response.ok) {
-                throw new Error('Failed to save user information.');
-              }
-              console.log('User information saved.');
-            })
-            .catch(error => {
-              console.error(error);
-            });
-        }}
-      >
-        Save
+        onClick={() => setShowUpdateUser(true)}
+        className="bg-violet-600 text-white font-bold rounded-lg px-8 py-4">
+        Update User
       </button>
 
+      {showUpdateUser && (
+        <UpdateUserModal
+          setShowConfirmationModal={setShowConfirmationModal}
+          closeModal={() => setShowUpdateUser(false)} />
+      )}
+
+      {showConfirmationModal && (
+        <ConfirmationModal closeModal={() => setShowConfirmationModal(false)} />
+      )}
 
     </div>
 
