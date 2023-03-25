80694af58c1965ee7c9ee1797ddc398b6de91431
Rubin Barclay <63505731+RubinBarclay@users.noreply.github.com>
2023-03-25 11:58:11 +0100

    feat: new shipment request for guests

--- src/components/NewShipmentForm/formSchema.ts
+++ src/components/NewShipmentForm/formSchema.ts
@@ -1,11 +1,11 @@
 import * as yup from "yup";
-
 export const schema = yup.object({
     reciverName: yup.string().required("This field is required"),
     weight: yup.number().required("This field is required"),
     destination: yup.number().required("This field is required"),
-    // boxColor: yup.string().required("Please select a color"),
-    // email: yup.string().required("This field is required for guests"),
+    email: yup.string().email().when("$isLoggedIn", ([isLoggedIn], schema) => (
+        isLoggedIn ? schema : schema.required()
+    ))
 })
 
 export type FormData = yup.InferType<typeof schema>;
---
+++ src/models/newShipmentGuest.ts
@@ -0,0 +1,9 @@
+interface NewShipmentGuest {
+    reciverName: string;
+    weight: number;
+    destinationID: number;
+    boxColor: string;
+    price: number;
+}
+
+export default NewShipmentGuest;
