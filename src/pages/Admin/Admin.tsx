2fb01853334dca1dca4bce58bb0c7cc3a1f90c4c
Rubin Barclay <63505731+RubinBarclay@users.noreply.github.com>
2023-03-27 11:27:24 +0200

    feat: reset admin search button

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,9 +1,11 @@
 import { useForm } from "react-hook-form";
-import React, { useState as useReactState, useEffect} from 'react';
+import React, { useState as useReactState, useEffect } from 'react';
 import ShipmentList from '../../components/ShipmentList/ShipmentList';
 import keycloak from "../../keycloak";
 import Shipment from '../../models/shipment';
 import { fetchCurrentShipmentsAdmin } from "../../api/shipments";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 
@@ -12,50 +14,63 @@ import { fetchCurrentShipmentsAdmin } from "../../api/shipments";
 const Admin = () => {
   const [currentShipments, setCurrentShipment] = useReactState<Shipment[]>([]);
   const [baseShipments, setBaseShipment] = useReactState<Shipment[]>([]);
-  const isAuthenticated = keycloak.authenticated;
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
   useEffect(() => {
-    if (isAuthenticated && keycloak && keycloak.tokenParsed && keycloak.tokenParsed.realm_access) {
-        const roles = keycloak.tokenParsed.realm_access.roles;
-        if(!roles.includes('ADMIN')) return;
+    if (keycloak.authenticated && keycloak && keycloak.tokenParsed && keycloak.tokenParsed.realm_access) {
+      const roles = keycloak.tokenParsed.realm_access.roles;
+      if (!roles.includes('ADMIN')) return;
 
-        getCurrentShipments();
+      getCurrentShipments();
     }
-    
+
   }, []);
 
 
   const getCurrentShipments = async () => {
     const shipments = await fetchCurrentShipmentsAdmin();
-    console.log(shipments);
     setCurrentShipment(shipments as Shipment[]);
     setBaseShipment(shipments as Shipment[]);
   }
-  const {register,handleSubmit, formState: {errors}} = useForm();
+
+
   const onSubmit = (id?: any) => {
-    if(id.id.length !== 0){
+    if (id.id.length !== 0) {
       setCurrentShipment(baseShipments.filter(shipment => {
         return shipment.id == id.id
-      }))}
-    else
-    {setCurrentShipment(currentShipments);}};
+      }))
+    }
+    else { setCurrentShipment(currentShipments); }
+  };
 
-    return (
-
-      <div className='w-5/6 h-5/6'>
-        <form onSubmit={ handleSubmit(onSubmit) }>
-          <fieldset>
-            <input {...register("id",{pattern:/^[0-9]*$/i})} className='shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-2/4 ' type="number" placeholder="Shipment ID" />
-            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold  px-4 py-3 rounded-full ml-5">
-              Search
-            </button>
-          </fieldset>      
-        </form>
-        <h2 className="text-2xl font-bold mt-5">Shipments</h2>
-        <ShipmentList shipments={currentShipments} setShipments={setCurrentShipment} />
-      </div>
-    )
-  
+  const resetShipments = () => {
+    setCurrentShipment(baseShipments);
   }
+
+  return (
+
+    <div className='w-5/6 h-5/6'>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex h-10">
+          <input 
+            {...register("id", { pattern: /^[0-9]*$/i })} 
+            min="0" 
+            type="number" 
+            placeholder="Shipment ID" 
+            className="shadow appearance-none border-none rounded-l-full pl-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-2/4" />
+          <button type="button" onClick={resetShipments} className="bg-white text-white font-bold pl-2 pr-4 rounded-r-full mr-2">
+            <FontAwesomeIcon icon={faXmark} color="gray" size="xl" />
+          </button>
+          <button type="submit" className="bg-violet-600 hover:bg-violet-700 text-white font-bold px-5 rounded-full">
+            Search
+          </button>
+        </div>
+      </form>
+      <h2 className="text-2xl font-bold mt-5">Shipments</h2>
+      <ShipmentList shipments={currentShipments} setShipments={setCurrentShipment} />
+    </div>
+  )
+
+}
 
 export default Admin
