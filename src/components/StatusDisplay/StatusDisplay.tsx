bc461483e24879619c2c54415046d78be3751526
Rubin Barclay <63505731+RubinBarclay@users.noreply.github.com>
2023-03-28 13:57:41 +0200

    fix: added status UNKNOWN for unknown status types

--- src/components/StatusDisplay/StatusDisplay.tsx
+++ src/components/StatusDisplay/StatusDisplay.tsx
@@ -35,8 +35,8 @@ function StatusDisplay({ statusList }: Props) {
         </span>
     ) : (
         <span className="relative inline-block px-3 py-1 font-semibold text-gray-900 leading-tight">
-            <span aria-hidden className="absolute inset-0 bg-gray-200 opacity-50 rounded-full"></span>
-            <span className="relative">{status}</span>
+            <span aria-hidden className="absolute inset-0 bg-violet-200 opacity-50 rounded-full"></span>
+            <span className="relative">UNKNOWN</span>
         </span>
     )
 }
